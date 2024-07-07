5856271a084a3f2edf38b57ae201e8276bc95b18
zekiblue <hi.zekiblue@gmail.com>
2024-07-07 02:41:42 +0200

    :recycle: Force dark mode

--- caddie_frontend/src/components/DarkModeButton/DarkModeButton.tsx
+++ caddie_frontend/src/components/DarkModeButton/DarkModeButton.tsx
@@ -1,4 +1,4 @@
-import { type FC } from "react";
+import { type FC, useEffect } from "react";
 
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 import { Button, useColorMode } from "@chakra-ui/react";
@@ -6,10 +6,17 @@ import { Button, useColorMode } from "@chakra-ui/react";
 const DarkModeButton: FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
+  useEffect(() => {
+    if (colorMode === "light") {
+      toggleColorMode();
+    }
+  }, [colorMode]);
+
   return (
-    <Button w={"40px"} h={"40px"} onClick={toggleColorMode} className="custom-button">
-      {colorMode === "light" ? <SunIcon fontSize={20} /> : <MoonIcon fontSize={17} />}
-    </Button>
+    <></>
+    // <Button w={"40px"} h={"40px"} onClick={toggleColorMode} className="custom-button">
+    //   {colorMode === "light" ? <SunIcon fontSize={20} /> : <MoonIcon fontSize={17} />}
+    // </Button>
   );
 };
 
