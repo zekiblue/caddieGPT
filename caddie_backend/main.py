43ea240356a22b4995b8afc2eaedbc76aff467d4
zekiblue <hi.zekiblue@gmail.com>
2024-07-07 12:22:50 +0200

    :sparkles: Latest backend

--- caddie_backend/main.py
+++ caddie_backend/main.py
@@ -10,26 +10,45 @@ from naptha_sdk.flow import Flow
 from naptha_sdk.user import generate_user
 import os
 
-
 app = FastAPI()
 
 
 @app.get("/votes")
 async def vote_results(daoCode: str, proposalId: str, walletAddress: str):
     naptha = await Naptha(
-          user=generate_user()[0],
-          hub_username=os.getenv("HUB_USER"), 
-          hub_password=os.getenv("HUB_PASS"), 
-          hub_url="ws://node.naptha.ai:3001/rpc",
-          node_url="http://node.naptha.ai:7001",
-      )
+        user=generate_user()[0],
+        hub_username=os.getenv("HUB_USER"),
+        hub_password=os.getenv("HUB_PASS"),
+        hub_url="ws://node.naptha.ai:3001/rpc",
+        node_url="http://node.naptha.ai:7001",
+    )
     flow_inputs = {"dao_code": daoCode, "proposal_id": proposalId, "wallet_address": walletAddress}
     flow = Flow(name="caddie_module", user_id=naptha.user["id"], worker_nodes=worker_nodes, module_params=flow_inputs)
-    task1 = Task(name="caddieGPT_vote", fn="caddie_module", worker_node=worker_nodes[0], orchestrator_node=naptha.node, flow_run=flow.flow_run)
-    response = await task1(dao_code=flow_inputs['dao_code'], proposal_id=flow_inputs['proposal_id'], wallet_address=flow_inputs['wallet_address'])
+    task1 = Task(name="caddieGPT_vote", fn="caddie_module", worker_node=worker_nodes[0], orchestrator_node=naptha.node,
+                 flow_run=flow.flow_run)
+    response = await task1(dao_code=flow_inputs['dao_code'], proposal_id=flow_inputs['proposal_id'],
+                           wallet_address=flow_inputs['wallet_address'])
     return response
-    
 
 
-
-    
+@app.post("/multiVotes")
+async def vote_results(daoCode: str, proposalIds: [str], walletAddress: str):
+    results = {}
+    for proposalId in proposalIds:
+        naptha = await Naptha(
+            user=generate_user()[0],
+            hub_username=os.getenv("HUB_USER"),
+            hub_password=os.getenv("HUB_PASS"),
+            hub_url="ws://node.naptha.ai:3001/rpc",
+            node_url="http://node.naptha.ai:7001",
+        )
+        flow_inputs = {"dao_code": daoCode, "proposal_id": proposalId, "wallet_address": walletAddress}
+        flow = Flow(name="caddie_module", user_id=naptha.user["id"], worker_nodes=worker_nodes,
+                    module_params=flow_inputs)
+        task1 = Task(name="caddieGPT_vote", fn="caddie_module", worker_node=worker_nodes[0],
+                     orchestrator_node=naptha.node,
+                     flow_run=flow.flow_run)
+        response = await task1(dao_code=flow_inputs['dao_code'], proposal_id=flow_inputs['proposal_id'],
+                               wallet_address=flow_inputs['wallet_address'])
+        results[proposalId] = response
+    return results
